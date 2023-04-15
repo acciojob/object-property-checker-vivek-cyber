2dcc613dca0133977fc0b97337b321d83c3b361d
Surya Vivek <51923160+vivek-cyber@users.noreply.github.com>
2023-04-16 03:51:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	if(sampleObject[key]==undefined)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
 }
 
 // Do not change the code below
